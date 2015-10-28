084dd59b80b37b1a5dc2b41f4c0bb7025831d878
Oliver Vartiainen <firoxer@gmail.com>
2015-10-28 19:19:04 +0200

    Add Mongoose & Co to dependencies and set them up
    Add a /locations path

--- server/server.js
+++ server/server.js
@@ -1,11 +1,35 @@
+var assert = require('assert');
+var config = require('config');
 var express = require('express');
+var mongoose = require('mongoose');
+
+var db = mongoose.connection;
+
+var locationSchema = new mongoose.Schema({
+  'category': String,
+  'location': {
+    'latitude': Number,
+    'longitude': Number
+  }
+});
+
+var Location = mongoose.model('Location', locationSchema);
+
+mongoose.connect(config.get('mongo').url);
 
 var app = express();
 
+// For debugging in development
 app.set('json spaces', 2);
 
 app.get('/', function(req, res) {
   res.json({});
+});
+
+app.get('/locations', function(req, res) {
+  Location.find(function(_err, locations) {
+    return res.json(locations);
+  });
 });
 
 var server = app.listen(8080, function() {
