4ef8abe5f333f16bfe7993f298ce57ef5b31cae4
Oliver Vartiainen <firoxer@gmail.com>
2015-10-30 22:01:27 +0200

    Add POST /events and accompanying stuff
    Add body-parser to dependencies as well

--- server/app/controllers-loader.js
+++ server/app/controllers-loader.js
@@ -5,6 +5,9 @@
  * @param m - Mongoose connection
  */
 module.exports = function (app, m) {
+  var bodyParser = require('body-parser');
+  app.use(bodyParser.urlencoded({extended: true}));
+
   app.get('/', function(req, res) {
     res.json({});
   });
@@ -12,6 +15,43 @@ module.exports = function (app, m) {
   app.get('/locations', function(req, res) {
     m.schemas.Location.find(req.query, function(_err, locations) {
       return res.json(locations);
+    });
+  });
+
+  app.get('/events', function(req, res) {
+    m.schemas.Event.find(req.query, function(_err, events) {
+      return res.json(events);
+    });
+  });
+
+  app.post('/events', function(req, res) {
+    var requiredFields = [
+      'latitude',
+      'longitude',
+      'name',
+      'originalID',
+      'url'
+    ];
+
+    var missingFields = requiredFields.reduce(function(mf, rf) {
+      return req.body[rf] === undefined ? mf.concat(rf) : mf;
+    }, []);
+
+    // Early exit in case of missing fields
+    if (missingFields.length !== 0) {
+      return res.json({'missingFields': missingFields});
+    }
+
+    var event = new m.schemas.Event({
+      'latitude': req.body.latitude,
+      'longitude': req.body.longitude,
+      'name': req.body.name,
+      'originalID': req.body.originalID,
+      'url': req.body.url
+    });
+
+    event.save(function(error, event) {
+      return res.json({'ok': error ? false : true}); // Umm
     });
   });
 };
--- server/app/mongoose-connection.js
+++ server/app/mongoose-connection.js
@@ -5,6 +5,17 @@ module.exports = (function() {
   var db = mongoose.connection;
   var schemas = {};
 
+  var eventSchema = new mongoose.Schema({
+    'category': String,
+    'coordinates': {
+      'latitude': Number,
+      'longitude': Number
+    },
+    'name': String,
+    'original_id': String,
+    'url': String
+  });
+
   var locationSchema = new mongoose.Schema({
     'category': String,
     'location': {
@@ -13,6 +24,7 @@ module.exports = (function() {
     }
   });
 
+  schemas.Event = mongoose.model('Event', eventSchema);
   schemas.Location = mongoose.model('Location', locationSchema);
 
   mongoose.connect(config.get('mongo').url);
