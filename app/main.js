1cd7a22b23133bdb11e46fbd19ef4bbe5a5bc6b1
Oliver Vartiainen <firoxer@gmail.com>
2015-11-14 19:49:02 +0200

    Add logging and tidy main.js

--- app/main.js
+++ app/main.js
@@ -1,29 +1,30 @@
 module.exports = function() {
   console.log('Initializing...');
 
-  // Initialization
-  let app = require('express')();
   let bodyParser = require('body-parser');
   let config = require('./config/config.js');
   let errorHandler = require('./errors/handler.js');
+  let express = require('express');
   let mongoose = require('mongoose');
-  let mongooseConnection =
-    require('./database/mongoose-connector.js')(config, mongoose);
+  let mongooseConnection = require('./database/mongoose-connector.js');
+  let morgan = require('morgan');
   let routeBinder = require('./routing/binder.js');
 
-  // Configuration
+  mongooseConnection(config, mongoose);
+
+  let app = express();
+
   app.use(bodyParser.urlencoded({extended: true}));
 
   if (config.ENVIRONMENT === 'development') {
     app.set('json spaces', 2);
+    app.use(morgan('dev'));
+  } else {
+    app.use(morgan('common'));
   }
 
-  // Route binding
   routeBinder(mongooseConnection, app);
-
-  // Error handling
   errorHandler(app);
 
-  // Booting
   app.listen(config.EXPRESS_PORT, () => console.log('Server up!'));
 };
