2cb8d98bdd12c6d754153536f94d7b14c7a79d34
Oliver Vartiainen <firoxer@gmail.com>
2015-12-05 00:23:17 +0200

    Tidy and normalize

---
+++ app/controllers/helpers.js
@@ -0,0 +1,30 @@
+module.exports = {
+  dropExcludedProperties: function(propertyList, originalObject) {
+    return propertyList.reduce(function(incompleteObject, property) {
+      incompleteObject[property] = originalObject[property];
+      return incompleteObject;
+    }, {});
+  },
+
+  requireNotVagueFlag: function(req) {
+    if (req.body.notVague === 'true') {
+      // Now the flag won't be left hanging around
+      req.body = {};
+    } else if (Object.getOwnPropertyNames(req.body).length === 0) {
+      return new Error('Possibly too vague: use notVague=true to enforce');
+    }
+
+    return null;
+  },
+
+  requireFields: function(req, requiredFields) {
+    let missingFields = requiredFields.reduce(function(mf, rf) {
+      return req.body[rf] === undefined ? mf.concat(rf) : mf;
+    }, []);
+
+    // Early exit in case of missing fields
+    if (missingFields.length !== 0) {
+      return new Error(`Missing fields: ${missingFields}`);
+    }
+  }
+};
