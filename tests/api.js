c92b3d3e5f2096620a26b640174e5514e28eeb71
Oliver Vartiainen <firoxer@gmail.com>
2016-02-17 20:11:47 +0200

    Tidy tests

--- tests/api.js
+++ tests/api.js
@@ -2,29 +2,15 @@ module.exports = function() {
   const DEFAULT_HOST = 'localhost';
   const DEFAULT_PORT = 8080;
 
-  const TESTING_EMAIL = 'testerman@test.test';
-  const TESTING_PASSWORD = 'testpassword';
+  const EMAIL = 'testerman@test.test';
+  const PASSWORD = 'testpassword';
 
   const frisby = require('frisby');
 
-  let credentials = encodeURIComponent(TESTING_EMAIL + ':' + TESTING_PASSWORD);
-  let host = process.env.VOLONTARIO_EXPRESS_HOST || DEFAULT_HOST;
-  let port = process.env.VOLONTARIO_EXPRESS_PORT || DEFAULT_PORT;
+  const credentials = encodeURIComponent(EMAIL + ':' + PASSWORD);
+  const host = process.env.VOLONTARIO_EXPRESS_HOST || DEFAULT_HOST;
+  const port = process.env.VOLONTARIO_EXPRESS_PORT || DEFAULT_PORT;
   const API_ROOT = `http://${credentials}@${host}:${port}`;
-
-  const userData = {
-    coordinates: {
-      latitude: 58.000,
-      longitude: 18.000
-    },
-    dateOfBirth: '1990-12-01',
-    email: TESTING_EMAIL,
-    familyName: 'User',
-    givenName: 'Testing',
-    password: TESTING_PASSWORD,
-    phoneNumber: '+35850000000',
-    tags: ['tagA', 'tagB', 'tagC']
-  };
 
   frisby.create('Make sure the API returns something')
     .get(API_ROOT)
@@ -32,6 +18,19 @@ module.exports = function() {
     .expectBodyContains('{}')
     .toss();
 
+  const userData = {
+    coordinates: {
+      latitude: 58.000,
+      longitude: 18.000
+    },
+    dateOfBirth: '1990-12-01',
+    email: EMAIL,
+    familyName: 'User',
+    givenName: 'Testing',
+    password: PASSWORD,
+    phoneNumber: '+35850000000',
+    tags: ['tagA', 'tagB', 'tagC']
+  };
   frisby.create('Create a testing user')
     .post(API_ROOT + '/users', userData)
     .expectStatus(201)
@@ -53,7 +52,6 @@ module.exports = function() {
         name: 'Testing Location',
         url: 'http://location-testing-url/'
       };
-
       frisby.create('Create a new location')
         .post(API_ROOT + '/locations', locationData)
         .expectStatus(201)
@@ -63,13 +61,14 @@ module.exports = function() {
           frisby.create('Fetch fresh location data and confirm it is the same')
             .get(`${API_ROOT}/locations/${locationId}`)
             .expectJSON(locationData)
+            .after(function() {
+              frisby.create('Delete testing locations')
+                .delete(API_ROOT + '/locations', {category: 'testing'})
+                .expectStatus(205)
+                .toss();
+            })
             .toss();
         })
-        .toss();
-
-      frisby.create('Delete testing locations')
-        .delete(API_ROOT + '/locations', {category: 'testing'})
-        .expectStatus(205)
         .toss();
 
       const eventData = {
@@ -92,14 +91,23 @@ module.exports = function() {
           frisby.create('Fetch fresh event data and confirm it is the same')
             .get(`${API_ROOT}/events/${eventId}`)
             .expectJSON(eventData)
+            .after(function() {
+              frisby.create('Delete testing events')
+                .delete(API_ROOT + '/events', {category: 'testing'})
+                .expectStatus(205)
+                .toss();
+            })
             .toss();
         })
         .toss();
-
-      frisby.create('Delete testing users')
-        .delete(API_ROOT + '/users', {email: TESTING_EMAIL})
-        .expectStatus(205)
-        .toss();
+    })
+    .after(function() {
+      setTimeout(function() {
+        frisby.create('Delete testing users')
+          .delete(API_ROOT + '/users', {email: EMAIL})
+          .expectStatus(205)
+          .toss();
+      }, 500);
     })
     .toss();
 };
